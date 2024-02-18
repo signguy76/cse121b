3e53133ab9b538d66e375d79b5db0b381ee1e126
signguy76 <coates.neal@gmail.com>
2024-02-17 22:27:03 -0800

    Drop down added

--- scripts/project.js
+++ scripts/project.js
@@ -16,7 +16,7 @@ function displayEmployees(workers) {
     reset();
     workers.forEach(employee => {
         const listItem = document.createElement("li");
-        listItem.textContent = `${employee.firstName} ${employee.lastName}`;
+        listItem.textContent = `Id# ${employee.id} | ${employee.firstName} ${employee.lastName} -- Title: ${employee.jobPosition} ---Location, ${employee.city}`;
         employeeList.appendChild(listItem);
     });
 }
@@ -25,14 +25,44 @@ function reset() {
     employeeList.innerHTML = "";
 }
 
-function filterData() {
-    let c = data.filter(employee => employee.city.includes("Camas"));
-    displayEmployees(c);
+// function filterData() {
+//     let c = data.filter(employee => employee.city.includes("Camas"));
+//     displayEmployees(c);
 
-}
+// }
+
+// Function: filterEmployee()
+const filterEmployee = (employee) => {
+    //reset();
+    const filter = document.getElementById("filtered").value;
+    switch (filter) {
+        case "camas":
+            displayEmployees(data.filter((employee) => employee.city.includes("Camas")));
+            break;
+        case "san jose":
+            displayEmployees(data.filter((employee) => employee.city.includes("San Jose")));
+            break;
+        case "anderson":
+            displayEmployees(data.filter((employee) => employee.city.includes("Anderson")));
+            break;
+        case "knoxville":
+            displayEmployees(data.filter((employee) => employee.city.includes("Knoxville")));
+            break;
+        case "vancouver":
+            displayEmployees(data.filter((employee) => employee.city.includes("Vancouver")));
+            break;
+        case "all":
+        default:
+            displayEmployees(data);
+            break;
+    }
+};
 
 
 
-document.querySelector("button").addEventListener("click", filterData);
 
+// Event Listener: filterEmployees Element change
+document.getElementById("filtered").addEventListener("change", () => {
+    filterEmployee(employeeList);
+});
 getEmployees(url);
